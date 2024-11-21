3833d7f682e6199cdc02e8a9d6f93a27431d918e
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2024-11-21 22:58:55 +0300

    Update module_3_hard.py
    вывела значения из словаря и добавила дроби

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,15 +1,15 @@
 def calculate_structure_sum(data_structure):
     summer = 0 # Счётчик
     for i in data_structure:
-        if isinstance(i, int):
+        if isinstance(i, int) or isinstance(i, float):
             summer += i
         elif isinstance(i, str):
             summer += len(i)
         elif isinstance(i, list) or isinstance(i, tuple) or isinstance(i, set):
             summer += calculate_structure_sum(i)  # Рекурсивный вызов
         elif isinstance(i, dict):
-            summer += sum(len(key) for key in i.keys() if isinstance(key, str)) # Ключи
-            summer += calculate_structure_sum(i.values())  # Рекурсивный вызов для значений
+            for key, value in i.items():
+                summer += calculate_structure_sum([key, value])
     return summer
 
 data_structure = [
